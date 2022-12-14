864dc5e32b8dd221771eddb9021affb482698f52
Daun Jeong <minori@mnetwork.co.kr>
2022-12-14 20:07:29 +0900

    Canvas background instead of image

--- static/js/main.js
+++ static/js/main.js
@@ -134,6 +134,31 @@
 				speed: 1000
 			});
 
+		
+		// Starfield
+		function getRandom(min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}           
+		var canvas = document.getElementById("starfield"),
+		context = canvas.getContext("2d"),
+		stars = 500,
+		colorrange = [0,60,240];
+
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+
+		for (var i = 0; i < stars; i++) {
+			var x = Math.random() * canvas.offsetWidth;
+			y = Math.random() * canvas.offsetHeight,
+			radius = Math.random() * 1.2,
+			hue = colorrange[getRandom(0,colorrange.length - 1)],
+			sat = getRandom(50,100);
+			context.beginPath();
+			context.arc(x, y, radius, 0, 360);
+			context.fillStyle = "hsl(" + hue + ", " + sat + "%, 88%)";
+			context.fill();
+		}
+
 	});
 
 })(jQuery);
